0f8f6e856c086ed528436f539bcbd6217ea9763a
MGarciav28 <mgarciavilchis@gmail.com>
2023-02-20 23:41:47 -0600

    Se crea funcion para modificar un registro ya existente

--- src/Modelo/ConsultasProducto.java
+++ src/Modelo/ConsultasProducto.java
@@ -15,7 +15,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class ConsultasProducto extends Conexion{
+    /*FUNCION PARA CONSULTAR PRODUCTOS*/
     public boolean registrar(Producto pro){
         PreparedStatement ps=null;
         Connection con=getConexion();
@@ -42,4 +44,35 @@ public class ConsultasProducto extends Conexion{
     }      
 }
     /* Fin de Registrar(Producto)*/
+    
+/*FUNCION PARA MODIFICAR PRODUCTOS*/    
+    
+public boolean modificar(Producto pro){
+    PreparedStatement ps =null;
+    Connection con =getConexion();
+    
+    String sql = "UPDATE producto SET codigo=?, nombre=?, precio=?, cantidad=? WHERE id=?";
+    
+    try{
+        ps=con.prepareStatement(sql);
+        ps.setString(1, pro.getCodigo());
+        ps.setString(2, pro.getNombre());
+        ps.setDouble(3, pro.getPrecio());
+        ps.setInt(4, pro.getCantidad());
+        ps.setInt(5, pro.getId());
+        ps.execute();
+        return true;
+    }catch(SQLException e){
+        System.err.println(e);
+        return false;
+    }finally{
+        try{
+            con.close();
+        }catch(SQLException e){
+            System.err.println(e);
+        }
+    }
+}
+
+/*FIN DE MODIFICAR PRODUCTO
 }
